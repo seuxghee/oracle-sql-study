333afe1d8161e391cbbad3ed53ce2566b46b8065
yangfaring <did030930@gmail.com>
2024-07-25 16:49:39 +0900

    Update query5.sql

--- 1day/query5.sql
+++ 1day/query5.sql
@@ -1,3 +1,6 @@
+query5
+문제: DATADICTIONARY D Table에서 최종자료 추출
+	
 select d.tablename, d.cname column_name, d.cdatatype data_type, decode(d.pkflag, 1, 'P') PK, decode(d.notnullflag, 1, 'NOT NULL') NULL_YN, d.memo
 from datadictionary d
 where clas = 88
