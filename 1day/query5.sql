8d021835d87558efe839d695263c0091cd6cdc80
yangfaring <did030930@gmail.com>
2024-07-25 16:46:15 +0900

    query5

---
+++ 1day/query5.sql
@@ -0,0 +1,10 @@
+select d.tablename, d.cname column_name, d.cdatatype data_type, decode(d.pkflag, 1, 'P') PK, decode(d.notnullflag, 1, 'NOT NULL') NULL_YN, d.memo
+from datadictionary d
+where clas = 88
+	and tablename in ('OPDACPT', 'PATMST')
+	and histseq = (select max(histseq)
+				   from datadictionary d2
+				   where d.clas = d2.clas
+				   		 and d.tablename = d2.tablename
+				   		 and d.cname = d2.cname)
+order by tablename, displayseq
